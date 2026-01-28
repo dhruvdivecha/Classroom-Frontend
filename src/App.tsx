1fe2866c8384cab5625584d5683861cbbd332e81
Dhruv Divecha <157246271+dhruvdivecha@users.noreply.github.com>
2026-01-28 12:24:27 +0300

    implement upload widget

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,12 @@ import { useNotificationProvider } from "./components/refine-ui/notification/use
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
 import Dashboard from "./pages/dashboard";
-import { BookOpen, Home } from "lucide-react";
+import { BookOpen, GraduationCap, Home } from "lucide-react";
 import { Layout } from "./components/refine-ui/layout/layout";
 import SubjectsList from "./pages/subjects/lists";
 import SubjectsCreate from "./pages/subjects/create";
+import ClassesList from "./pages/classes/lists";
+import ClassesCreate from "./pages/classes/create";
 
 function App() {
   return (
@@ -43,9 +45,14 @@ function App() {
                   name: 'subjects',
                   list: '/subjects',
                   create: '/subjects/create',
-                  // edit: '/subjects/edit/:id',
-                  // show: '/subjects/show/:id',
                   meta: { label: 'Subjects', icon: <BookOpen /> }
+
+                },
+                {
+                  name: 'classes',
+                  list: '/classes',
+                  create: '/classes/create',
+                  meta: { label: 'Classes', icon: <GraduationCap /> }
 
                 }
               ]}
@@ -57,9 +64,13 @@ function App() {
                   </Layout>
                 }>
                   <Route path="/" element={<Dashboard />} />
-                  <Route path="/subjects">
+                  <Route path="subjects">
                     <Route index element={<SubjectsList />} />
                     <Route path="create" element={<SubjectsCreate />} />
+                  </Route>
+                  <Route path="classes">
+                    <Route index element={<ClassesList />} />
+                    <Route path="create" element={<ClassesCreate />} />
                   </Route>
                 </Route>
               </Routes>
