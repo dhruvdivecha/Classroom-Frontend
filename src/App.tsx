977d462da0939e031ad782cd411496a15fa2a282
Dhruv Divecha <157246271+dhruvdivecha@users.noreply.github.com>
2026-02-12 07:40:13 +0300

    Implement class-details

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import SubjectsList from "./pages/subjects/lists";
 import SubjectsCreate from "./pages/subjects/create";
 import ClassesList from "./pages/classes/lists";
 import ClassesCreate from "./pages/classes/create";
+import ClassesShow from "./pages/classes/show";
 
 function App() {
   return (
@@ -52,6 +53,7 @@ function App() {
                   name: 'classes',
                   list: '/classes',
                   create: '/classes/create',
+                  show: '/classes/show/:id',
                   meta: { label: 'Classes', icon: <GraduationCap /> }
 
                 }
@@ -71,6 +73,7 @@ function App() {
                   <Route path="classes">
                     <Route index element={<ClassesList />} />
                     <Route path="create" element={<ClassesCreate />} />
+                    <Route path="show/:id" element={<ClassesShow />} />
                   </Route>
                 </Route>
               </Routes>
